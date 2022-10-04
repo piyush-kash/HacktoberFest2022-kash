fbbb747265b66a7ce19080328ef08bc865f7ffe3
Shashank Singh <57489764+shashanksgh@users.noreply.github.com>
2022-10-04 20:46:06 +0530

    Create reverseStringRecursive.cpp

---
+++ C++/reverseStringRecursive.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void reverse(string &str)
+{
+   stack<char> st;
+   for (int i=0; i<str.length(); i++)
+       st.push(str[i]);
+ 
+   for (int i=0; i<str.length(); i++) {
+       str[i] = st.top();
+       st.pop();
+   }      
+}
+ 
+//main function
+int main()
+{
+    string str = "geeksforgeeks";
+    reverse(str);
+    cout << str;
+    return 0;
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
